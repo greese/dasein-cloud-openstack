a260e8715943e3d59c0d56aebe4193d63e2ea0f7
Danielle Mayne <danielle.mayne@enstratus.com>
2014-08-22 16:23:30 +0100

    handle JSON errors if image tag is not a JSONObject

--- src/main/java/org/dasein/cloud/openstack/nova/os/network/Quantum.java
+++ src/main/java/org/dasein/cloud/openstack/nova/os/network/Quantum.java
@@ -539,7 +539,7 @@ public class Quantum extends AbstractVLANSupport {
     @Nonnull
     @Override
     public Collection<InternetGateway> listInternetGateways(@Nullable String vlanId) throws CloudException, InternalException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return Collections.emptyList();
     }
 
     @Override
